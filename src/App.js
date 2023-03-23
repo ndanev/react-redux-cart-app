e2661d09cf969fd90c0c76f1379614def842c8a0
Nemanja Danev <nemanja.danev.93@gmail.com>
2023-03-22 22:34:15 -0400

    moving send request logic to the redux

--- src/App.js
+++ src/App.js
@@ -3,21 +3,35 @@ import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
 
 import { useEffect } from 'react';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+
+import Notification from './components/UI/Notification';
+import { sendCartData } from './store/cartSlice';
+
+let isInitial = true;
 
 function App() {
+  const dispatch = useDispatch()
   const showCart = useSelector(state => state.ui.cartIsVisible)
   const cart = useSelector(state => state.cart)
+  const notification = useSelector(state => state.ui.notification)
 
   useEffect(() => {
-    fetch('https://react-food-order-app-57dfe-default-rtdb.firebaseio.com/cart.json', { method: 'PUT', body: JSON.stringify(cart) })
-  }, [cart])
+    if (isInitial) {
+      isInitial = false
+      return
+    }
+    dispatch(sendCartData(cart))
+  }, [cart, dispatch])
 
   return (
-    <Layout>
-      {showCart && <Cart />}
-      <Products />
-    </Layout>
+    <>
+      {notification && <Notification status={notification.status} title={notification.title} message={notification.message} />}
+      <Layout>
+        {showCart && <Cart />}
+        <Products />
+      </Layout>
+    </>
   );
 }
 
--- src/store/cartSlice.js
+++ src/store/cartSlice.js
@@ -1,4 +1,5 @@
 import { createSlice } from '@reduxjs/toolkit';
+import { uiActions } from './uiSlice';
 
 const cartSlice = createSlice({
     name: 'cart',
@@ -40,6 +41,42 @@ const cartSlice = createSlice({
         },
     },
 });
+
+export const sendCartData = (cart) => {
+    return async (dispatch) => {
+        dispatch(uiActions.showNotification({
+            status: 'pending',
+            title: 'Sending...',
+            message: 'Sending cart data!'
+        }))
+
+        const url = 'https://react-food-order-app-57dfe-default-rtdb.firebaseio.com/cart.json'
+
+        const sendRequest = async () => {
+            const response = await fetch(url, { method: 'PUT', body: JSON.stringify(cart) })
+
+            if (!response.ok) {
+                throw new Error('Sending cart data failed!')
+            }
+        }
+
+        try {
+            await sendRequest()
+            dispatch(uiActions.showNotification({
+                status: 'success',
+                title: 'Success!',
+                message: 'Sent cart data successfully!'
+            }))
+
+        } catch (error) {
+            dispatch(uiActions.showNotification({
+                status: 'error',
+                title: 'Error!',
+                message: 'Sending cart data failed!'
+            }))
+        }
+    }
+}
 
 export const cartActions = cartSlice.actions;
 
